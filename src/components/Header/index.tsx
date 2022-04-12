cacee7eb70eed667ccd985dd652e012986e93378
Nigel Janssens <janssensnigel@gmail.com>
2022-04-12 14:56:24 +0200

    debug missing href when clicking IBS logo in the Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -13,16 +13,23 @@ const Header: FC = () => {
     return (
         <header className="flex justify-between items-center lg:mx-auto bg-gradient-to-b from-zinc-900 to-zinc-800/90 text-zinc-100">
             <div className="flex flex-grow max-w-screen-2xl justify-between items-center mx-auto py-6 px-4 sm:px-6 lg:px-8 ">
-                <div className="flex gap-x-5 hover:cursor-pointer ">
-                    <Image
-                        src="/logo.png"
-                        alt="IBS Logo"
-                        width={30}
-                        height={30}
-                        objectFit="contain"
-                    />
+                <div className="flex hover:cursor-pointer ">
                     <Link href="#" passHref>
-                        <Typography weight="medium" variant="h3" component="a">
+                        <Image
+                            src="/logo.png"
+                            alt="IBS Logo"
+                            width={30}
+                            height={30}
+                            objectFit="contain"
+                        />
+                    </Link>
+                    <Link href="#" passHref>
+                        <Typography
+                            weight="medium"
+                            variant="h3"
+                            component="a"
+                            className="px-5"
+                        >
                             Isatis Business Solutions
                         </Typography>
                     </Link>
