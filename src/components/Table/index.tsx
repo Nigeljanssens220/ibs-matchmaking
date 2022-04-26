1d97a06e21e1e99e7c8e89f16b3ca977e5c3a8c0
Nigel Janssens <janssensnigel@gmail.com>
2022-04-26 17:22:07 +0200

    remove border

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -44,19 +44,19 @@ const Table: FC<TableProps> = ({ items, headers }) => {
                                                     : ''
                                             }
                                         >
-                                            <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 border border-black">
+                                            <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 ">
                                                 {item.job_title}
                                             </td>
-                                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500 border border-black">
+                                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500 ">
                                                 {item.source}
                                             </td>
-                                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500 border border-black">
+                                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500 ">
                                                 {item.employer}
                                             </td>
-                                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500 border border-black ">
+                                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500 ">
                                                 {item.weekly_hours}
                                             </td>
-                                            <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6 border border-black">
+                                            <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6 ">
                                                 <a
                                                     href={item.job_url}
                                                     className="text-indigo-600 hover:text-indigo-900"
