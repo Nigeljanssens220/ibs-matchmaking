e117fb7ba2eeb35bac6172607302653a3d18c22f
Nigel Janssens <nigel.janssens@isatis-business-solutions.nl>
2022-05-09 13:06:13 +0200

    move data fetching to ItemList (probably revert this later)

--- src/pages/dashboard/resumes.tsx
+++ src/pages/dashboard/resumes.tsx
@@ -1,60 +1,10 @@
 import Dropbox from '@/components/Dropbox'
 import DashboardLayout from '@/components/Layout/Dashboard'
-import List from '@/components/List'
+import ItemList from '@/components/List'
 import Typography from '@/components/Typography'
-import { Resume } from '@/types/database'
-import { fetcher } from '@/utils/fetch'
 import type { NextPage } from 'next'
-import { useSession } from 'next-auth/react'
-import { useEffect, useState } from 'react'
-import useSWRImmutable from 'swr/immutable'
 
 const Resumes: NextPage = () => {
-    const { data: session, status } = useSession()
-    const [resumes, setResumes] = useState<Resume[]>([])
-
-    const { data, error, mutate } = useSWRImmutable(
-        status !== 'authenticated'
-            ? null
-            : [
-                  'https://ibs-matchmaking-api.azurewebsites.net/readItems',
-                  session!.accessToken,
-              ],
-        fetcher
-    )
-
-    // Some data transformations when data is collected or updated by SWR
-    // useEffect(() => {
-    //     if (data) {
-    //         const modifiedData = data.projects.map((project: Project) => {
-    //             return {
-    //                 ...project,
-    //                 upload_date: new Date(
-    //                     project.upload_date * 1000
-    //                 ).toLocaleDateString(),
-    //                 submission_deadline: new Date(
-    //                     project.submission_deadline * 1000
-    //                 ).toLocaleDateString(),
-    //                 job_title: project.job_title
-    //                     .replace('/', ' / ')
-    //                     .replace('  ', ' '),
-    //                 location: project.location
-    //                     .replace('/', ' / ')
-    //                     .replace('  ', ' '),
-    //             }
-    //         })
-    //         setProjects(modifiedData)
-    //     }
-    //     return () => {
-    //         setProjects([])
-    //     }
-    // }, [data])
-
-    useEffect(() => {
-        if (data) {
-            console.log(data.items)
-        }
-    }, [data])
     return (
         <DashboardLayout>
             <div className="min-h-screen max-w-screen-2xl flex flex-col w-full p-3 lg:p-6 xl:px-8 ">
@@ -80,13 +30,11 @@ const Resumes: NextPage = () => {
                             </Typography>
                         </div>
                     </div>
-                    <div className="py-20 space-y-5">
+
+                    <div className="flex flex-col py-20 space-y-3 md:space-y-4 items-center ">
                         <Dropbox />
                     </div>
-                    <List
-                        data={resumes}
-                        headers={['File', 'Description', 'Skills', 'Status']}
-                    />
+                    <ItemList />
                 </div>
             </div>
         </DashboardLayout>
