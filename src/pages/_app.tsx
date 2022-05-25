89b912750c65e7fd57e0b56f1fa643c49740d9ea
Nigel Janssens <nigel.janssens@isatis-business-solutions.nl>
2022-05-25 19:01:15 +0200

    Add hot toast status notification during resume upload

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,19 @@
-import '../styles/globals.css'
-import Head from 'next/head'
 import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { Toaster } from 'react-hot-toast'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <>
-            <SessionProvider session={pageProps.session}>
-                <Head>
-                    <title>Matchmaking - Isatis Business Solutions</title>
-                    <link rel="favicon" href="/favicon.ico" />
-                </Head>
-                <Component {...pageProps} />
-            </SessionProvider>
-        </>
+        <SessionProvider session={pageProps.session}>
+            <Toaster />
+            <Head>
+                <title>Matchmaking - Isatis Business Solutions</title>
+                <link rel="favicon" href="/favicon.ico" />
+            </Head>
+            <Component {...pageProps} />
+        </SessionProvider>
     )
 }
 
